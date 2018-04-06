31640275cb9d72e0d862049926b394fc525a4662
Dharmang Bhavsar <dharmangb@gmail.com>
2018-04-06 14:56:17 -0400

    Delete j2py_install.sh

--- Code Conversion/j2py_install.sh
+++
@@ -1,9 +0,0 @@
-wget http://www.antlr3.org/download/antlr-3.1.3.tar.gz
-tar xfz antlr-3.1.3.tar.gz
-cd antlr-3.1.3/runtime/Python/
-python setup.py install
-
-wget https://github.com/downloads/natural/java2python/java2python-0.5.1.tar.gz
-tar xfz java2python-0.5.1.tar.gz
-cd java2python
-python setup.py install
